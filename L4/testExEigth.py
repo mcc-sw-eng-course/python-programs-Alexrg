89da284d3b781bde5e0a895867fb22401ed66123
alexrg <alerodriguezgarcia@me.com>
2019-03-06 08:56:19 -0600

    Lecture four

--- L4/testExEigth.py
+++ L4/testExEigth.py
@@ -1,13 +1,16 @@
 import unittest
-import exEigth
+from exEigth import mean
 
 class TestExEight(unittest.TestCase):
 	"""
 	Create tests
 	"""
+	
+	dataTest = [1, 2, 3, 4, 5, 6]
+	data = [6, 7, 2, 3, 8, 4, 5, 6, 6, 7, 1, 3]
+
 	def equal_mean(self):
-		dataTest = [1,2,3,4,5,6]
-		data = [6, 7, 2, 3, 8, 4, 5, 6, 6, 7, 1, 3]
+		
 		meanTest = mean(dataTest)
 		mean = mean(data)
 		self.assertNotEqual(meanTest, mean)
