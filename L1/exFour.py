acadeb4c9f7aa0441861dec369090e39c5c9c327
alexrg <alerodriguezgarcia@me.com>
2019-03-06 08:10:55 -0600

    Lecture four

--- L1/exFour.py
+++ L1/exFour.py
@@ -26,7 +26,7 @@ def calculate_deviations(data, mean, n):
 	variance = 0
 
 	for x in data:
-		data_deviation += math.pow(x - mean,2)
+		data_deviation += (x - mean)*(x-mean)
 
 	variance = data_deviation/n
 	std_deviation = round(math.sqrt(variance),2)
@@ -36,5 +36,4 @@ def calculate_deviations(data, mean, n):
 student_avg = calculate_mean(student_grades, n)
 student_deviations = calculate_deviations(student_grades, student_avg, n)
 
-print("The student's average: {}".format(student_avg))
 print("The student's deviation: {}".format(student_deviations))
