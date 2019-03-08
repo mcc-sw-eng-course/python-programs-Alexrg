b57e2499ebe26cc9b42acf4df5f70020b3dde1aa
alexrg <alerodriguezgarcia@me.com>
2019-03-07 21:05:27 -0600

    Lecture six

--- L6/exTwentynine.py
+++ L6/exTwentynine.py
@@ -6,17 +6,21 @@ Implement the method
 This methods sorts the data contained in the file specified. Define custom exceptions or
 error codes for situations where there may be special errors
 """
+# Crear una lista con números y tamaño aleatorio
+def create_list():
+	list = []
+	list_length = random.randint(0, 200)
+	for i in range(0,list_length):
+		list.append(random.randint(0, 100))
+	print (list)
+	return list
 
-# La lista que se odernará
-list = [0, 4, 5, 103, 98, 35, 86, 7, 1, 1, 100, 3, 0, 45, 5, 3, 19, 35, 86, 7, 1, 1, 100, 3]
-list_length = len(list)
-new_list = []
-print(list)
-pivot_number = 0
+list_two = create_list()
+list_two_length = len(list_two)
 
 # 1. Elegir un elemento del arreglo de elementos a ordenar, al que llamaremos pivote.
-def choose_pivot_number(list):
-	pivot_index = random.randint(0, list_length-1)
+def choose_pivot_number(list, length):
+	pivot_index = random.randint(0, length)
 	pivot_number = list[pivot_index]
 	return pivot_number
 
@@ -44,13 +48,16 @@ def quick_sort(list, list_length, pivot_number):
 	
 	return new_list
 
-sort = quick_sort(list, list_length, pivot_number)
+
+pivot_number = choose_pivot_number(list_two, list_two_length)
+
+sort = quick_sort(list_two, list_two_length, pivot_number)
 print(sort)
 # 3. Repetir este proceso de forma recursiva para cada sublista mientras éstas contengan más
 # de un elemento. Una vez terminado este proceso todos los elementos estarán ordenados.
-if (sort == list.sort()):
+if (sort == list_two.sort()):
 	print("La lista ya está ordenada")
 else:
-	sort = quick_sort(list, list_length, pivot_number)
+	sort = quick_sort(list_two, list_two_length, pivot_number)
 	print(sort)
 
